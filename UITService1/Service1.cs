0819b4d8cfff9348a28e15e298e7101741d54fb1
unknown <buixuanthaivtabs@gmail.com>
2021-04-06 05:54:43 +0700

    first commit

---
+++ UITService1/Service1.cs
@@ -0,0 +1,119 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Data;
+//using System.Diagnostics;
+//using System.Linq;
+//using System.ServiceProcess;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace UITService1
+//{
+//    public partial class Service1 : ServiceBase
+//    {
+//        public Service1()
+//        {
+//            InitializeComponent();
+//        }
+
+//        protected override void OnStart(string[] args)
+//        {
+//        }
+
+//        protected override void OnStop()
+//        {
+//        }
+//    }
+//}
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+namespace UITService1
+{
+    public partial class Service1 : ServiceBase
+    {
+        int time = 0;
+        Timer timer = new Timer(); // name space(using System.Timers;) 
+        public Service1()
+        {
+            InitializeComponent();
+        }
+        protected override void OnStart(string[] args)
+        {
+            WriteToFile("Service is started at " + DateTime.Now);
+            timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
+            timer.Interval = 5000; //number in milisecinds 
+            timer.Enabled = true;
+        }
+        private bool IsProcessRunning(string ProcessName)
+        {
+            foreach (Process clsProcess in Process.GetProcesses())
+            {
+                if (clsProcess.ProcessName.Contains(ProcessName))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        protected override void OnStop()
+        {
+            WriteToFile("Service is stopped at " + DateTime.Now);
+        }
+        private void OnElapsedTime(object source, ElapsedEventArgs e)
+        {
+            //WriteToFile("Service is recall at " + DateTime.Now);
+            Process[] p = Process.GetProcessesByName("Notepad");
+            Process a = new Process();
+            a.StartInfo.FileName = "notepad.exe";
+            time++;
+            if (time % 2 == 0)
+            {
+                a.Start();
+            }
+            else
+            {
+                foreach (Process t in p)
+                {
+                    t.Kill();
+                }
+            }
+
+
+
+        }
+        public void WriteToFile(string Message)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\Logs\\ServiceLog_" + DateTime.Now.Date.ToShortDateString().Replace('/', '_') + ".txt";
+            if (!File.Exists(filepath))
+            {
+                // Create a file to write to. 
+                using (StreamWriter sw = File.CreateText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+        }
+    }
+}
